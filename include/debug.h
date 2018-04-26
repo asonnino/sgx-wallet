ac11d267377f0a841509a77a323106eb8b171c3a
asonnino <alberto.sonnino@ucl.ac.uk>
2018-04-26 20:18:27 +0100

    Fixed bug (potential recursive include)

--- include/debug.h
+++ include/debug.h
@@ -3,7 +3,12 @@
 // 
 //
 // ============================================================================
+#ifndef DEBUG_H_
+#define DEBUG_H_
 
 
 // uncomment to enable debug print
 #define ENCLAVE_DEBUG
+
+
+#endif // DEBUG_H_
