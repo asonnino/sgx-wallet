fb8fae7995ad8d70191d80a857ba0e3e79e5157e
asonnino <alberto.sonnino@ucl.ac.uk>
2018-05-03 20:49:55 +0100

    Typo fix

--- enclave/sealing/sealing.h
+++ enclave/sealing/sealing.h
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with SGX-WALLET.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef SEALING_H_
 #define SEALING_H_
